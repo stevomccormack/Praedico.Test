a0d08637f95089d5454ea24a09e26dd5c8c47b1c
Steve McCormack <steve@mweb.co>
2024-11-26 17:42:38 +1100

    Small revisions to BookingQueryableExtensions

--- Praedico.Bookings.Application/Bookings/BookingQueryableExtensions.cs
+++ Praedico.Bookings.Application/Bookings/BookingQueryableExtensions.cs
@@ -3,52 +3,45 @@ using Praedico.Bookings.Domain.Schedules;
 
 namespace Praedico.Bookings.Application.Bookings;
 
-    public static class BookingQueryableExtensions
-    {
-        public static IQueryable<Booking> OverlapsWith(this IQueryable<Booking> query, DateTimeRange range)
-        {
-            return query.Where(x => 
-                x.TimeRange.Start < range.End && 
-                x.TimeRange.End > range.Start);
-        }
+public static class BookingQueryableExtensions
+{
+    public static IQueryable<Booking> OverlapsWith(this IQueryable<Booking> query, DateTimeRange range) =>
+        query.Where(x =>
+            x.TimeRange.Start < range.End &&
+            x.TimeRange.End > range.Start);
 
-        public static IQueryable<Booking> IsWithin(this IQueryable<Booking> query, DateTimeRange range)
-        {
-            return query.Where(x => 
-                x.TimeRange.Start <= range.Start && 
+    public static IQueryable<Booking> IsWithin(this IQueryable<Booking> query, DateTimeRange range) =>
+        query.Where(x =>
+                x.TimeRange.Start <= range.Start &&
                 x.TimeRange.End >= range.End);
-        }
 
-        public static IQueryable<Booking> CollidesWith(this IQueryable<Booking> query, DateTimeRange range)
-        {
-            return query.Where(x =>
-                (x.TimeRange.Start <= range.Start && x.TimeRange.End >= range.End) || // StartsBeforeAndEndsAfter
-                (x.TimeRange.Start <= range.Start && x.TimeRange.End > range.Start && x.TimeRange.End <= range.End) || // StartsBeforeAndEndsDuring
-                (x.TimeRange.Start >= range.Start && x.TimeRange.Start < range.End && x.TimeRange.End >= range.End) || // StartsDuringAndEndsAfter
-                (x.TimeRange.Start >= range.Start && x.TimeRange.End <= range.End)); // IsWithin
-        }
+    public static IQueryable<Booking> CollidesWith(this IQueryable<Booking> query, DateTimeRange range) =>
+        query.Where(x =>
+            x.TimeRange.Start <= range.Start && x.TimeRange.End >= range.End || // StartsBeforeAndEndsAfter
+            x.TimeRange.Start <= range.Start && x.TimeRange.End > range.Start && x.TimeRange.End <= range.End || // StartsBeforeAndEndsDuring
+            x.TimeRange.Start >= range.Start && x.TimeRange.Start < range.End && x.TimeRange.End >= range.End || // StartsDuringAndEndsAfter
+            x.TimeRange.Start >= range.Start && x.TimeRange.End <= range.End); // IsWithin
 
-        public static IQueryable<Booking> AvailableCars(this IQueryable<Booking> query, 
-            DateTime pickupDateTime,
-            DateTime returnDateTime,
-            string[]? carTypes)
-        {
-            query = query.Where(x => x.Car.IsActive);
-
-            // Apply car type filter if specified
-            if (carTypes?.Length > 0){
-                var carTypesList = carTypes.ToList();
-                query = query.Where(x => carTypesList.Any(carType => carType == x.Car.CarType.Name));
-            }
-
-            // Exclude cars that collide with the provided time range
-            query = query.Where(x =>
-                !(x.PickupDateTime <= pickupDateTime && x.ReturnDateTime >= returnDateTime) && // Not StartsBeforeAndEndsAfter
-                !(x.PickupDateTime <= pickupDateTime && x.ReturnDateTime > pickupDateTime && x.ReturnDateTime <= returnDateTime) && // Not StartsBeforeAndEndsDuring
-                !(x.PickupDateTime >= pickupDateTime && x.PickupDateTime < returnDateTime && x.ReturnDateTime >= returnDateTime) && // Not StartsDuringAndEndsAfter
-                !(x.PickupDateTime >= pickupDateTime && x.ReturnDateTime <= returnDateTime)); // Not IsWithin
-
-            return query;
-        }
+    public static IQueryable<Booking> AvailableCars(this IQueryable<Booking> query,
+        DateTime pickupDateTime,
+        DateTime returnDateTime,
+        string[]? carTypes)
+    {
+        query = query.Where(x => x.Car.IsActive);
+        query = query.ByCarTypes(carTypes);
+        query = query.ExcludeCollisions(pickupDateTime, returnDateTime);
+        return query;
     }
 
+    public static IQueryable<Booking> ByCarTypes(this IQueryable<Booking> query, string[]? carTypes) =>
+        carTypes?.Length > 0 ? query.Where(x => carTypes.Contains(x.Car.CarType.Name)) : query;  //DOESTNT WORK WITH VALUECONVERTER! NEED VALUE OBJECT!
+        //carTypes?.Length > 0 ? query.Where(x => carTypes.Any(ct => ct == x.Car.CarType.Name)) : query;
+        //carTypes?.Length > 0 ? query.AsEnumerable().Where(x => carTypes.Contains(x.Car.CarType.Name)).AsQueryable() : query;    
+
+    public static IQueryable<Booking> ExcludeCollisions(this IQueryable<Booking> query, DateTime pickupDateTime, DateTime returnDateTime) =>
+        query.Where(x =>
+            !(x.PickupDateTime <= pickupDateTime && x.ReturnDateTime >= returnDateTime) && // Not StartsBeforeAndEndsAfter
+            !(x.PickupDateTime <= pickupDateTime && x.ReturnDateTime > pickupDateTime && x.ReturnDateTime <= returnDateTime) && // Not StartsBeforeAndEndsDuring
+            !(x.PickupDateTime >= pickupDateTime && x.PickupDateTime < returnDateTime && x.ReturnDateTime >= returnDateTime) && // Not StartsDuringAndEndsAfter
+            !(x.PickupDateTime >= pickupDateTime && x.ReturnDateTime <= returnDateTime)); // Not IsWithin
+}
